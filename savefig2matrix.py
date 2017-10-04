4ffd07d3d3a64b6c9484db616e5b9213b4cd238c
simonsleo <403400998@qq.com>
2017-10-04 22:40:56 +0200

    Update savefig2matrix.py

--- savefig2matrix.py
+++ savefig2matrix.py
@@ -12,3 +12,20 @@ fig.canvas.draw()
 # Now we can save it to a numpy array.
 data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
 data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+#------------------------------------
+
+
+import io
+from PIL import Image
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot([1, 2])
+plt.title("test")
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+buf.seek(0)
+im = Image.open(buf)
+im.show()
+buf.close()
